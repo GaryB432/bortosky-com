b80e95a02537513d4c1785f97847d46ff426bb35
Gary Bortosky <gary@bortosky.com>
2015-04-25 12:42:28 -0500

    add productio n data service test

--- test/tests.ts
+++ test/tests.ts
@@ -18,6 +18,39 @@ describe('testing Math service', function () {
     });
 });
 
+describe('ProductionDataService', function () {
+    var svc: ProductionDataService;
+
+    beforeEach(function () {
+        module('app.services');
+
+        inject(function (_ProductionDataService_: ProductionDataService) {
+            svc = _ProductionDataService_;
+        });
+    });
+
+    it('should get shows properly.', function () {
+
+        inject(($httpBackend: ng.IHttpBackendService) => {
+            $httpBackend.expect('GET', 'producers.json')
+                .respond(200, '[{"name": "P1","productions": [{ "show": "S1", "opening": "1999-11-12T20:00:00-06:00", "role": "R1" },{ "show": "S2", "opening": "2005-11-11T20:00:00-06:00", "role": "R2" }]},{"name": "P2","productions": [{ "show": "S3", "opening": "2010-07-30T20:00:00-05:00", "role":"R3" }]}]');
+
+            var expectedShows = [
+                { show: 'S1', opening: new Date('1999-11-12T20:00:00-06:00'), producer: 'P1', role: 'R1' },
+                { show: 'S2', opening: new Date('2005-11-11T20:00:00-06:00'), producer: 'P1', role: 'R2' },
+                { show: 'S3', opening: new Date('2010-07-30T20:00:00-05:00'), producer: 'P2', role: 'R3' }
+            ];
+
+            svc.getProductions().then((resultShows) => {
+                expect(resultShows).toEqual(expectedShows);
+            });
+
+            $httpBackend.flush();
+
+        });
+    });
+});
+
 describe('array flattener',() => {
     it('should flatten correctly',() => {
         expect(Utilities.flatten([[1, 2], [3, 4], [5, 6]])).toEqual([1, 2, 3, 4, 5, 6]);
