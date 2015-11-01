4c6e8fa6df59b0f9fd465bdcbc055befb3276c0e
Gary Bortosky <garybort@gmail.com>
2015-11-01 13:25:24 -0600

    update bower and remove angulartics

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,6 @@
             'app/bower_components/angular-animate/angular-animate.js',
             'app/bower_components/angular-route/angular-route.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
-            'app/bower_components/angulartics/dist/angulartics.min.js',
-            'app/bower_components/angulartics/dist/angulartics-ga.min.js',
             'app/gary/js/main.js',
             'test/**/*.js'
         ]
--- source/gary/js/app.ts
+++ source/gary/js/app.ts
@@ -1,17 +1,13 @@
 ﻿/// <reference path="../../../typings/tsd.d.ts" />
 angular.module("app",
     [
-        "angulartics",
-        "angulartics.google.analytics",
         "ngRoute",
         "app.services",
         "app.controllers",
         "app.directives"
-    ]).config(["$routeProvider", "$analyticsProvider", ($routeProvider: ng.route.IRouteProvider, $analyticsProvider: any): void => {
+    ]).config(["$routeProvider", ($routeProvider: ng.route.IRouteProvider): void => {
         $routeProvider
             .when("/", { templateUrl: "templates/states/main.html" })
             .when("/theater", { templateUrl: "templates/states/theater.html", controller: "TheaterCtrl" })
             .otherwise({ redirectTo: "/" });
-        $analyticsProvider.firstPageview(true); /* Records pages that don't use $state or $route */
-        $analyticsProvider.withAutoBase(true);  /* Records full path */
     }]);
