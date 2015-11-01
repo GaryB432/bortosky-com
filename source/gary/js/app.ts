6316278893e9f7d0a4ec86c0e61a0e18d1d0239f
Gary Bortosky <garybort@gmail.com>
2015-11-01 03:02:17 -0600

    cleanup about route

--- source/gary/js/app.ts
+++ source/gary/js/app.ts
@@ -10,7 +10,6 @@ angular.module("app",
     ]).config(["$routeProvider", "$analyticsProvider", ($routeProvider: ng.route.IRouteProvider, $analyticsProvider: any): void => {
         $routeProvider
             .when("/", { templateUrl: "templates/states/main.html" })
-            .when("/about", { templateUrl: "templates/states/about.html" })
             .when("/theater", { templateUrl: "templates/states/theater.html", controller: "TheaterCtrl" })
             .otherwise({ redirectTo: "/" });
         $analyticsProvider.firstPageview(true); /* Records pages that don't use $state or $route */
