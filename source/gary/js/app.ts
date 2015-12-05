552ef158800f8d33a827a8a968d9ee47c7ab1dd5
Gary Bortosky <garybort@gmail.com>
2015-12-04 21:26:19 -0600

    added typedefs

--- source/gary/js/app.ts
+++ source/gary/js/app.ts
@@ -1,8 +1,6 @@
 ﻿/// <reference path="../../../typings/tsd.d.ts" />
 /// <reference path="models" />
 
-/* tslint:disable:typedef */
-
 angular.module("app", [
     "ngRoute",
     "app.services",
@@ -22,33 +20,38 @@ namespace Google {
         years: IAnnualCount[];
     }
 
+    enum MapLoadState {
+        READY,
+        NOT_READY
+    }
+
     export class Viz {
-        private started: Promise<string>;
+        private started: Promise<MapLoadState>;
 
         private infoAccepted: Promise<IChartModel>;
 
         public start(): void {
-            this.started = new Promise<string>((resolve, reject) => {
+            this.started = new Promise<MapLoadState>((resolve: (value?: MapLoadState) => void, reject: (error?: any) => void) => {
                 google.load("visualization", "1.0", { packages: ["corechart"] });
                 google.setOnLoadCallback(() => {
                     console.log("started");
-                    resolve("ready");
+                    resolve(MapLoadState.READY);
                 });
             });
-            console.log("starting....");
+            console.log("starting...");
             this.draw();
         }
         public acceptData(elem: HTMLElement, years: IAnnualCount[]): void {
-            this.infoAccepted = new Promise<IChartModel>((resolve, reject) => {
+            this.infoAccepted = new Promise<IChartModel>((resolve: (value?: IChartModel) => void, reject: (error?: any) => void) => {
                 resolve({ element: elem, years: years });
             });
             this.draw();
         }
         private draw(): void {
-            this.started.then((r) => {
-                console.log(r);
+            this.started.then((r: MapLoadState) => {
+                console.log(MapLoadState[r]);
                 if (this.infoAccepted) {
-                    this.infoAccepted.then((j) => {
+                    this.infoAccepted.then((j: IChartModel) => {
                         j.element.innerText = j.years.length.toString();
                         console.log(j);
                         console.log(this.createDataTable(j.years));
