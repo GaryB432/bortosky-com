554ac08193dadb43e0d0ba439b389ae83fb25cc5
Gary Bortosky <garybort@gmail.com>
2016-09-25 00:22:09 -0500

    a child module that gives dup name warnings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { GaryModule } from './gary/gary.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { GaryComponent } from './gary/gary.component';
-import { TheaterComponent } from './gary/theater/theater.component';
-import { OverviewComponent } from './gary/overview/overview.component';
-import { ProductionService } from './gary/theater/production.service';
-import { CustomDatePipe } from './shared';
-import { routing } from './app.routing';
+import { appRouting } from './app.routing';
 
 import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
 
@@ -20,19 +17,16 @@ import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
     BrowserModule,
     HttpModule,
     FormsModule,
-    routing
+    RouterModule,
+    GaryModule,
+    appRouting
   ],
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
-    GaryComponent,
-    TheaterComponent,
-    OverviewComponent,
-    CustomDatePipe
+    AboutComponent
   ],
   providers: [
-    ProductionService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/gary/gary.module.ts
@@ -0,0 +1,50 @@
+import { NgModule, ApplicationRef } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// import { HttpModule } from '@angular/http';
+// import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { GaryComponent } from './gary.component';
+import { TheaterComponent } from './theater/theater.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ProductionService } from './theater/production.service';
+import { garyRouting } from './gary.routing';
+import { CustomDatePipe } from '../shared';
+
+// import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
+
+@NgModule({
+  imports: [
+    RouterModule,
+    BrowserModule,
+    garyRouting
+  ],
+  declarations: [
+    GaryComponent,
+    TheaterComponent,
+    OverviewComponent,
+    CustomDatePipe,
+  ],
+  providers: [
+    ProductionService
+  ],
+  exports: [GaryComponent]
+})
+export class GaryModule {
+  constructor(public appRef: ApplicationRef) { }
+  hmrOnInit(store) {
+    console.log('HMR Gary store', store);
+  }
+  //   hmrOnDestroy(store) {
+  //     let cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
+  //     // recreate elements
+  //     store.disposeOldHosts = createNewHosts(cmpLocation);
+  //     // remove styles
+  //     removeNgStyles();
+  //   }
+  //   hmrAfterDestroy(store) {
+  //     // display new elements
+  //     store.disposeOldHosts();
+  //     delete store.disposeOldHosts;
+  //   }
+}
