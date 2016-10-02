ab35cf11bb2b81b84c41a95498aa9229533a4a97
Gary Bortosky <garybort@gmail.com>
2016-10-02 14:26:29 -0500

    cleanup

--- src/app/gary/gary.module.ts
+++ src/app/gary/gary.module.ts
@@ -1,7 +1,5 @@
 import { NgModule, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { HttpModule } from '@angular/http';
-// import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { GaryComponent } from './gary.component';
@@ -11,8 +9,6 @@ import { ChartComponent } from './theater/chart.component';
 import { ProductionService } from './theater/production.service';
 import { garyRouting } from './gary.routing';
 import { CustomDatePipe } from '../shared';
-
-// import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
 
 @NgModule({
   imports: [
@@ -34,19 +30,4 @@ import { CustomDatePipe } from '../shared';
 })
 export class GaryModule {
   constructor(public appRef: ApplicationRef) { }
-  public hmrOnInit(store: any): void {
-    console.log('HMR Gary store', store);
-  }
-  //   hmrOnDestroy(store) {
-  //     let cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
-  //     // recreate elements
-  //     store.disposeOldHosts = createNewHosts(cmpLocation);
-  //     // remove styles
-  //     removeNgStyles();
-  //   }
-  //   hmrAfterDestroy(store) {
-  //     // display new elements
-  //     store.disposeOldHosts();
-  //     delete store.disposeOldHosts;
-  //   }
 }
