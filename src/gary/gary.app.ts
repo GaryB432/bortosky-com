98e76a610084d41cdf9af767ba096a5ca2e2cf9c
Gary Bortosky <garybort@gmail.com>
2021-09-10 11:08:12 -0500

    add qr ts

--- src/gary/gary.app.ts
+++ src/gary/gary.app.ts
@@ -12,10 +12,62 @@ export function getVCardUrl(vcard: VCard): URL {
   return u;
 }
 
+interface QROptions {
+  src: string;
+  from: string;
+  caption: string;
+}
+
+export function addQR(options: QROptions, n = 0) {
+  const qrs = document.querySelector<HTMLDivElement>('.qrs');
+
+  if (qrs) {
+    console.log(qrs);
+  }
+
+  const f = `<div class="qr">
+  <img
+    id="contact${n}"
+    src="${options.src}"
+    alt="contact"
+    data-from="${options.from}"
+  />
+  <div class="caption">${options.caption}</div>
+</div>`;
+
+const d = new DocumentFragment();
+const da = document.createElement('div');
+da.classList.add('qr');
+const img = document.createElement('img');
+const cap = document.createElement('div');
+
+da.appendChild(img);
+da.appendChild(cap);
+d.appendChild(da);
+
+
+
+
+}
+
 function main(e?: Event) {
   if (e) {
     console.log(e.eventPhase);
   }
+
+  //   <img
+  //   id="contact"
+  //   class="qr"
+  //   src="qr/minimal-a.png"
+  //   alt="contact"
+  //   data-href="https://www.qrcode-monkey.com/#"
+  // />
+
+  addQR({
+    src: 'qr/minimal-a.png',
+    from: 'https://www.qrcode-monkey.com/#',
+    caption: 'here',
+  });
   const qrimg = document.querySelector<HTMLImageElement>('#contact.qr-not');
   if (qrimg) {
     qrimg.src = getVCardUrl([
