d25b4c6a1d29a013d4f5654a2b9b61562862c782
Gary Bortosky <garybort@gmail.com>
2021-09-10 11:27:12 -0500

    add zxing qr

--- src/gary/gary.app.ts
+++ src/gary/gary.app.ts
@@ -1,5 +1,11 @@
 type VCard = string[];
 
+interface QROptions {
+  src: string;
+  from: string;
+  caption: string;
+}
+
 export function getVCardUrl(vcard: VCard): URL {
   const chl = ['BEGIN:VCARD', 'VERSION:3.0', ...vcard, 'END:VCARD'].join('\n');
 
@@ -12,71 +18,48 @@ export function getVCardUrl(vcard: VCard): URL {
   return u;
 }
 
-interface QROptions {
-  src: string;
-  from: string;
-  caption: string;
-}
-
 export function addQR(options: QROptions, n = 0) {
   const qrs = document.querySelector<HTMLDivElement>('.qrs');
 
   if (qrs) {
-    console.log(qrs);
+    const f = new DocumentFragment();
+    const dqr = document.createElement('div');
+    dqr.classList.add('qr');
+    const img = document.createElement('img');
+    img.setAttribute('id', `contact${n}`);
+    img.setAttribute('src', options.src);
+    img.setAttribute('alt', `contact qr ${n}`);
+    img.setAttribute('title', options.from);
+    const cap = document.createElement('div');
+    cap.classList.add('caption');
+    cap.textContent = options.caption;
+
+    dqr.appendChild(img);
+    dqr.appendChild(cap);
+    f.appendChild(dqr);
+    qrs.appendChild(f);
   }
-
-  const f = `<div class="qr">
-  <img
-    id="contact${n}"
-    src="${options.src}"
-    alt="contact"
-    data-from="${options.from}"
-  />
-  <div class="caption">${options.caption}</div>
-</div>`;
-
-const d = new DocumentFragment();
-const da = document.createElement('div');
-da.classList.add('qr');
-const img = document.createElement('img');
-const cap = document.createElement('div');
-
-da.appendChild(img);
-da.appendChild(cap);
-d.appendChild(da);
-
-
-
-
 }
 
-function main(e?: Event) {
-  if (e) {
-    console.log(e.eventPhase);
-  }
-
-  //   <img
-  //   id="contact"
-  //   class="qr"
-  //   src="qr/minimal-a.png"
-  //   alt="contact"
-  //   data-href="https://www.qrcode-monkey.com/#"
-  // />
-
-  addQR({
-    src: 'qr/minimal-a.png',
-    from: 'https://www.qrcode-monkey.com/#',
-    caption: 'here',
-  });
-  const qrimg = document.querySelector<HTMLImageElement>('#contact.qr-not');
-  if (qrimg) {
-    qrimg.src = getVCardUrl([
-      'N:Gary Bortosky',
-      'TEL:13146090415',
-      'URL:bortosky.com/gary',
-      'EMAIL:gary@bortosky.com',
-    ]).toString();
-  }
+function main() {
+  const qrs: QROptions[] = [
+    {
+      src: 'qr/minimal-a.png',
+      from: 'https://www.qrcode-monkey.com/#',
+      caption: 'contact a',
+    },
+    {
+      src: getVCardUrl([
+        'N:Gary Bortosky',
+        'TEL:13146090415',
+        'URL:bortosky.com/gary',
+        'EMAIL:gary@bortosky.com',
+      ]).toString(),
+      from: 'https://zxing.org',
+      caption: 'contact b',
+    },
+  ];
+  qrs.forEach(addQR);
 }
 
 if (document.readyState === 'loading') {
