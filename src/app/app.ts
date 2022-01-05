50303c1816f7c4426fd4c22ebeee4f4dff1c9ab5
Gary Bortosky <garybort@gmail.com>
2022-01-05 13:12:16 -0600

    stub moon json

---
+++ src/app/app.ts
@@ -0,0 +1,44 @@
+export const h = 5;
+
+/*
+
+20220117T065100Z pm
+Full Wolf Moon
+
+20220216T115900Z am
+Full Snow Moon
+
+20220318T032000Z am
+Full Worm Moon
+
+20220416T025700Z pm
+Full Pink Moon
+
+20220516T121500Z am
+Full Flower Moon
+
+20220614T075200Z am
+Full Strawberry Moon
+
+20220713T023800Z pm
+Full Buck Moon
+
+20220811T093600Z pm
+Full Sturgeon Moon
+
+20220910T055800Z am
+Full Corn Moon
+
+20221009T045400Z pm
+Full Hunter’s Moon
+
+20221108T060200Z am
+Full Beaver Moon
+
+20221207T110900Z pm
+Full Cold Moon
+*/
+
+const m = {
+
+}
