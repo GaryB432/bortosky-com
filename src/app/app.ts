d1df939be08c8169489450da278149fd357f4094
Gary Bortosky <garybort@gmail.com>
2022-01-05 13:14:54 -0600

    fix pm

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ export const h = 5;
 
 /*
 
-20220117T065100Z pm
+20220117T185100Z
 Full Wolf Moon
 
 20220216T115900Z am
@@ -11,7 +11,7 @@ Full Snow Moon
 20220318T032000Z am
 Full Worm Moon
 
-20220416T025700Z pm
+20220416T145700Z
 Full Pink Moon
 
 20220516T121500Z am
@@ -20,23 +20,24 @@ Full Flower Moon
 20220614T075200Z am
 Full Strawberry Moon
 
-20220713T023800Z pm
+20220713T143800Z
 Full Buck Moon
 
-20220811T093600Z pm
+20220811T213600Z
 Full Sturgeon Moon
 
 20220910T055800Z am
 Full Corn Moon
 
-20221009T045400Z pm
+20221009T165400Z
 Full Hunter’s Moon
 
 20221108T060200Z am
 Full Beaver Moon
 
-20221207T110900Z pm
+20221207T230900Z
 Full Cold Moon
+
 */
 
 const m = {
