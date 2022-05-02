33d1318edcb5a0ee7e4f5f2799cc1aca35a04093
Gary Bortosky <garybort@gmail.com>
2022-05-02 18:22:52 -0500

    add theater resume

--- src/routes/gary/theater/index.svelte
+++ src/routes/gary/theater/index.svelte
@@ -1,13 +1,43 @@
 <script context="module" lang="ts">
 	import Crumbs from '$lib/Crumbs.svelte';
 	import Footer from '$lib/Footer.svelte';
-	import regionMap from './theater.json';
-	export const prerender = true;
+	import data from './theater.json';
+
+	type ISODate = string;
+
+	interface Production {
+		opening: ISODate;
+		producer: Producer;
+		role: string;
+		show: string;
+	}
+
+	interface Producer {
+		name: string;
+		productions: Production[];
+	}
+
+	const dprods = data as { producers: Producer[] };
+
+	const prods = dprods.producers
+		.map((producer) => {
+			producer.productions.forEach((production) => (production.producer = producer));
+			return producer.productions;
+		})
+		.flat(1)
+		.map((s) => {
+			const dd = new Date(s.opening);
+			return {
+				...s,
+				openDate: new Date(s.opening)
+			};
+		})
+		.sort((a, b) => b.opening.localeCompare(a.opening));
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Bortosky Family" />
+	<title>Theater</title>
+	<meta name="description" content="Gary Theater" />
 </svelte:head>
 
 <div class="bort-container mdl-grid">
@@ -16,14 +46,61 @@
 		class="bort-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col"
 	>
 		<Crumbs />
-		<h2>Bortosky Family</h2>
-		<p>The only information available about the fam at this time is Gary's</p>
-		<li><a href="gary">Gary</a></li>
-		{regionMap.producers.length}
+		<h2>Theater Resumé</h2>
+		<table class="shows">
+			<thead>
+				<tr>
+					<th>Show</th>
+					<th>Opening</th>
+					<th>Company</th>
+					<th>Role</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each prods as prod}
+					<tr>
+						<td>{prod.show}</td>
+						<td>
+							<div title={prod.openDate.toLocaleString()}>
+								{new Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(
+									prod.openDate
+								)}
+							</div>
+						</td>
+						<td>{prod.producer.name}</td>
+						<td>{prod.role}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 </div>
 
 <Footer />
 
-<style>
+<style lang="scss">
+	div {
+		.theater {
+			margin: 10px;
+			h2 {
+				font-size: 120%;
+				font-weight: 700;
+				margin-bottom: 6px;
+			}
+		}
+	}
+
+	table {
+		&.shows {
+			border-spacing: 0;
+			border-collapse: collapse;
+			font-size: 80%;
+			td,
+			th {
+				padding: 6px;
+				text-align: left;
+				border: thin solid silver;
+			}
+		}
+	}
 </style>
