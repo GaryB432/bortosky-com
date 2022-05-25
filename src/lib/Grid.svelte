c4bfaeb329668b6d0d9bde4ad67a4fa1d8d720e5
Gary Bortosky <garybort@gmail.com>
2022-05-25 12:02:33 -0500

    stub calendar route

---
+++ src/lib/Grid.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Crumbs from './Crumbs.svelte';
+</script>
+
+<div class="bort-container mdl-grid">
+  <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone" />
+  <div
+    class="bort-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col"
+  >
+    <Crumbs />
+    <slot />
+  </div>
+</div>
---
+++ src/routes/calendar/year/[start].svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+  import Crumbs from '$lib/Crumbs.svelte';
+  import Grid from '$lib/Grid.svelte';
+  import Footer from '$lib/Footer.svelte';
+  export const prerender = true;
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Bortosky Family" />
+</svelte:head>
+
+<Grid>
+  <h2>Bortosky Family</h2>
+  <p>calendar coming upt</p>
+</Grid>
+
+<Footer />
+
+<style>
+</style>
