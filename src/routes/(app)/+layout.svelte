5808b7828b63522cc2b7d595717ad10514e5bc54
Gary Bortosky <garybort@gmail.com>
2023-11-09 16:24:29 -0600

    clean up comments

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -17,86 +17,9 @@
   </div>
 </main>
 
-<!-- 
-<style>
-  .left {
-    min-width: 35ch;
-  }
-
-  .left a {
-    display: flex;
-    height: 5em;
-    align-content: normal;
-    justify-content: center;
-    align-items: center;
-    gap: 1em;
-    border-block-end: 2px solid #283a94;
-  }
-
-  .left h1 {
-    margin-bottom: 0;
-  }
-
-  @media screen and (min-width: 992px) {
-    main {
-      display: grid;
-      grid-template-columns: 1fr 4fr;
-      height: 100%;
-    }
-    .left a {
-      padding: 1em;
-      justify-content: center;
-      align-items: center;
-    }
-
-    .left h1 {
-      font-size: 2rem;
-    }
-
-    .right {
-      border-left: 2px solid var(--sand-3);
-      padding: 4em;
-    }
-  }
-</style> -->
-
-<!-- 
-
-
-<style lang="scss">
-  main {
-    margin: 0 1rem;
-  }
-  .top {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    h1 {
-      padding: 0;
-      margin: 0;
-      font-size: 1.5rem;
-      line-height: unset;
-
-    }
-
-    .left {
-      display: flex;
-      gap: 1rem;
-      align-items: center;
-    }
-  }
-</style>
-
-
-
- -->
-
 <style lang="scss">
   .top {
     --color-h1: white;
-    // --color-document-text: white;
-    // color: var(--color-h1);
     background-color: var(--fun-blue);
     .left {
       display: flex;
--- src/routes/(app)/also-burgers/+page.svelte
+++ src/routes/(app)/also-burgers/+page.svelte
@@ -1,23 +1,5 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-
   let opened = false;
-
-  onMount(() => {
-    // var icon = document.getElementById("icon");
-    // var icon1 = document.getElementById("a");
-    // var icon2 = document.getElementById("b");
-    // var icon3 = document.getElementById("c");
-    // var nav = document.getElementById("nav");
-    // var blue = document.getElementById("blue");
-    // icon.addEventListener("click", function () {
-    //   icon1.classList.toggle("a");
-    //   icon2.classList.toggle("c");
-    //   icon3.classList.toggle("b");
-    //   nav.classList.toggle("show");
-    //   blue.classList.toggle("slide");
-    // });
-  });
 </script>
 
 <svelte:head>
