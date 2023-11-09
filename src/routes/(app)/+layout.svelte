7d03e95bc9b7bb0428e0139798f15efc2103b1e1
Gary Bortosky <garybort@gmail.com>
2023-11-09 16:26:34 -0600

    Feature/renew layout (#10)
    * add some burgers
    
    * clean up styles
    
    * update navigation
    
    * clean up comments

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,85 +1,47 @@
 <script>
   import FamilyLogo from "$lib/components/FamilyLogo.svelte";
-  import Navigation from "$lib/components/Navigation.svelte";
+  import SlideNav from "$lib/components/SlideNav.svelte";
+
 </script>
 
 <main>
   <div class="top">
     <div class="left">
+      <SlideNav />
       <FamilyLogo />
       <h1>Bortosky Family</h1>
     </div>
-    <Navigation />
   </div>
   <div>
     <slot />
   </div>
 </main>
 
-<!-- 
-<style>
-  .left {
-    min-width: 35ch;
-  }
-
-  .left a {
-    display: flex;
-    height: 5em;
-    align-content: normal;
-    justify-content: center;
-    align-items: center;
-    gap: 1em;
-    border-block-end: 2px solid #283a94;
-  }
-
-  .left h1 {
-    margin-bottom: 0;
-  }
-
-  @media screen and (min-width: 992px) {
-    main {
-      display: grid;
-      grid-template-columns: 1fr 4fr;
-      height: 100%;
-    }
-    .left a {
-      padding: 1em;
-      justify-content: center;
-      align-items: center;
-    }
-
-    .left h1 {
-      font-size: 2rem;
-    }
-
-    .right {
-      border-left: 2px solid var(--sand-3);
-      padding: 4em;
-    }
-  }
-</style> -->
-
 <style lang="scss">
-  main {
-    margin: 0 1rem;
-  }
   .top {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    h1 {
-      padding: 0;
-      margin: 0;
-      font-size: 1.5rem;
-      line-height: unset;
-
-    }
-
+    --color-h1: white;
+    background-color: var(--fun-blue);
     .left {
       display: flex;
-      gap: 1rem;
       align-items: center;
+      h1 {
+        margin: 0;
+      }
     }
+  }
+  @media screen and (min-width: 576px) {
+    /* landscape phones */
+  }
+  @media screen and (min-width: 768px) {
+    /* tablets */
+  }
+  @media screen and (min-width: 992px) {
+    /* desktops */
+  }
+  @media screen and (min-width: 1200px) {
+    /* large desktops */
+  }
+  @media screen and (min-width: 1400px) {
+    /* larger desktops */
   }
 </style>
---
+++ src/routes/(app)/burgers/+page.svelte
@@ -0,0 +1,256 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let autoShow: ReturnType<typeof setTimeout>;
+
+  onMount(() => {
+    let currentActive = 0;
+    const checkboxes =
+      document.querySelectorAll<HTMLInputElement>(".grid input");
+    autoShow = setInterval(() => {
+      const covert4 = currentActive % 4;
+      checkboxes[covert4].checked = !checkboxes[covert4].checked;
+      if (!checkboxes[covert4].checked) {
+        currentActive += 1;
+      }
+    }, 1000);
+  });
+</script>
+
+<svelte:head>
+  <title>bortosky-com - burgers</title>
+</svelte:head>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  class="grid"
+  on:click={() => {
+    clearInterval(autoShow);
+  }}
+>
+  <div class="menu cross menu--1">
+    <label>
+      <input type="checkbox" />
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="30" />
+        <path class="line--1" d="M0 40h62c13 0 6 28-4 18L35 35" />
+        <path class="line--2" d="M0 50h70" />
+        <path class="line--3" d="M0 60h62c13 0 6-28-4-18L35 65" />
+      </svg>
+    </label>
+  </div>
+  <div class="menu cross menu--2">
+    <label>
+      <input type="checkbox" />
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="30" />
+        <path class="line--1" d="M0 70l28-28c2-2 2-2 7-2h64" />
+        <path class="line--2" d="M0 50h99" />
+        <path class="line--3" d="M0 30l28 28c2 2 2 2 7 2h64" />
+      </svg>
+    </label>
+  </div>
+  <div class="menu back menu--3">
+    <label>
+      <input type="checkbox" />
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="30" />
+        <path class="line--1" d="M0 40h62c18 0 18-20-17 5L31 55" />
+        <path class="line--2" d="M0 50h80" />
+        <path class="line--3" d="M0 60h62c18 0 18 20-17-5L31 45" />
+      </svg>
+    </label>
+  </div>
+  <div class="menu back menu--4">
+    <label>
+      <input type="checkbox" />
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="30" />
+        <path class="line--1" d="M0 55l14-10c4.7-3.3 9-5 13-5h72" />
+        <path class="line--2" d="M0 50h99" />
+        <path class="line--3" d="M0 45l14 10c4.7 3.3 9 5 13 5h72" />
+      </svg>
+    </label>
+  </div>
+</div>
+
+<style lang="scss">
+  input {
+    display: none;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+    width: 100vw;
+    height: 80vh;
+    position: absolute;
+  }
+
+  .grid > * {
+    position: relative;
+  }
+
+  label {
+    display: block;
+    cursor: pointer;
+    position: absolute;
+    width: 50vw;
+    height: 50vw;
+    max-width: 150px;
+    max-height: 150px;
+  }
+
+  @media (min-aspect-ratio: 1/1) {
+    label {
+      width: 50vh;
+      height: 50vh;
+    }
+  }
+
+  .menu--1 label,
+  .menu--2 label {
+    bottom: 0;
+  }
+
+  .menu--3 label,
+  .menu--4 label {
+    top: 0;
+  }
+
+  .menu--1 label,
+  .menu--3 label {
+    right: 0;
+  }
+
+  .menu--2 label,
+  .menu--4 label {
+    left: 0;
+  }
+
+  path {
+    fill: none;
+    stroke: #ffffff;
+    stroke-width: 3;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    --length: 24;
+    --offset: -38;
+    stroke-dasharray: var(--length) var(--total-length);
+    stroke-dashoffset: var(--offset);
+    transition: all 0.8s cubic-bezier(0.645, 0.045, 0.355, 1);
+  }
+
+  circle {
+    fill: #fff3;
+    opacity: 0;
+  }
+
+  label:hover circle {
+    opacity: 1;
+  }
+
+  .cross input:checked + svg {
+    .line--1,
+    .line--3 {
+      --length: 22.627416998;
+    }
+    .line--2 {
+      --length: 0;
+    }
+  }
+
+  .back input:checked + svg {
+    .line--1,
+    .line--3 {
+      --length: 8.602325267;
+    }
+  }
+
+  .menu--1 {
+    background-color: #3f77e9;
+    .line--1,
+    .line--3 {
+      --total-length: 126.64183044433594;
+    }
+    .line--2 {
+      --total-length: 70;
+    }
+    input:checked + svg {
+      .line--1,
+      .line--3 {
+        --offset: -94.1149185097;
+      }
+      .line--2 {
+        --offset: -50;
+      }
+    }
+  }
+
+  .menu--2 {
+    background-color: #0095f9;
+    .line--1,
+    .line--3 {
+      --total-length: 111.22813415527344;
+      --offset: -50.22813415527344;
+    }
+    .line--2 {
+      --total-length: 99;
+    }
+    input:checked + svg {
+      path {
+        transform: translateX(30px);
+      }
+      .line--1,
+      .line--3 {
+        --offset: -16.9705627485;
+      }
+      .line--2 {
+        --offset: -20;
+      }
+    }
+  }
+
+  .menu--3 {
+    background-color: #00aef6;
+    .line--1,
+    .line--3 {
+      --total-length: 126.38166809082031;
+    }
+    .line--2 {
+      --total-length: 80;
+    }
+    input:checked + svg {
+      .line--1,
+      .line--3 {
+        --offset: -109.1770175568;
+      }
+    }
+  }
+
+  .menu--4 {
+    background-color: #18bee5;
+    .line--1,
+    .line--3 {
+      --total-length: 103.35061645507812;
+      --offset: -42.35061645507812;
+    }
+    .line--2 {
+      --total-length: 99;
+    }
+    input:checked + svg {
+      path {
+        transform: translateX(31px);
+      }
+      .line--1,
+      .line--3 {
+        --offset: -8.602325267;
+      }
+      .line--2 {
+        --offset: -7;
+      }
+    }
+  }
+</style>
