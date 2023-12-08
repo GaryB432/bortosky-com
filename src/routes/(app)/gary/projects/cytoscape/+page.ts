f26f6ca7d3a758a7b301069b3c0d67e3c369bcc4
Gary Bortosky <garybort@gmail.com>
2023-12-08 16:55:28 -0600

    move cyto page

--- src/routes/(app)/gary/projects/cytoscape/+page.ts
+++ src/routes/(app)/gary/projects/cytoscape/+page.ts
@@ -2,11 +2,12 @@ import { base } from "$app/paths";
 import { getElements } from "$lib/project/cyto";
 import type { GaryProject } from "$lib/project/project";
 import type { PageLoad } from "./$types";
+import {page} from "$app/stores"
 
-export const load = (async ({ fetch }) => {
-  const response = await fetch(base.concat("/gary/projects.json"));
-  const info: GaryProject[] = await response.json();
-  const elements = await getElements(info);
+
+export const load = (async (asdf) => {
+  const ff = await asdf.parent();
+  const elements = await getElements(ff.projects);
 
   return { elements };
 }) satisfies PageLoad;
