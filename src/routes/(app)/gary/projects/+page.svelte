6473e3bc0d92a8b3067851a06e185fa3598520c1
Gary Bortosky <garybort@gmail.com>
2023-12-09 11:57:10 -0600

    add domains

--- src/routes/(app)/gary/projects/+page.svelte
+++ src/routes/(app)/gary/projects/+page.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
+  import ProjectCard from "$lib/components/ProjectCard.svelte";
   import type { PageData } from "./$types";
 
   export let data: PageData;
+
+  $: projects = data.projects;
 </script>
 
-<div>
-  {JSON.stringify(data)}
+<div class="container">
+  <div class="projects">
+    {#each projects as project}
+      <ProjectCard {project}></ProjectCard>
+    {/each}
+  </div>
 </div>
+
+<style>
+  .projects {
+    margin: 2em auto;
+    width: 70vw;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+  }
+</style>
--- src/routes/(app)/gary/projects/cytoscape/+page.ts
+++ src/routes/(app)/gary/projects/cytoscape/+page.ts
@@ -1,13 +1,9 @@
-import { base } from "$app/paths";
 import { getElements } from "$lib/project/cyto";
-import type { GaryProject } from "$lib/project/project";
 import type { PageLoad } from "./$types";
-import {page} from "$app/stores"
 
-
-export const load = (async (asdf) => {
-  const ff = await asdf.parent();
-  const elements = await getElements(ff.projects);
+export const load = (async ({ parent }) => {
+  const { projects } = await parent();
+  const elements = await getElements(projects);
 
   return { elements };
 }) satisfies PageLoad;
