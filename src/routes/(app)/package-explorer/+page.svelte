d9866008d40430e38012cfed70c40aec1487e313
Gary Bortosky <garybort@gmail.com>
2024-08-13 19:01:27 -0500

    stub package explorer route

---
+++ src/routes/(app)/package-explorer/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  let { data }: { data: PageData } = $props();
+</script>
+
+<svelte:head>
+  <title>bortosky-com - package-explorer</title>
+</svelte:head>
+
+<article class="container">
+  {#each data.paramPackages as paramPkg}
+    <h2>
+      {paramPkg.name}
+    </h2>
+    <div>{paramPkg.description}</div>
+    <div class="keywords">
+      {#each paramPkg.keywords as k}
+        <div class="keyword">{k}</div>
+      {/each}
+    </div>
+  {/each}
+</article>
+
+<style lang="scss">
+  .container {
+    padding: 1em;
+    border: thin solid silver;
+  }
+  .keywords {
+    display: flex;
+    flex-direction: row;
+    gap: 0.25rem;
+  }
+  .keyword {
+    display: inline-block;
+    color: white;
+    padding: 0.5rem;
+    border-radius: 5px;
+    background-color: rgb(117, 106, 137);
+  }
+  @media screen and (min-width: 576px) {
+    /* landscape phones */
+  }
+  @media screen and (min-width: 768px) {
+    /* tablets */
+  }
+  @media screen and (min-width: 992px) {
+    /* desktops */
+  }
+  @media screen and (min-width: 1200px) {
+    /* large desktops */
+  }
+  @media screen and (min-width: 1400px) {
+    /* larger desktops */
+  }
+</style>
---
+++ src/routes/(app)/package-explorer/+page.ts
@@ -0,0 +1,38 @@
+import { error } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+type Packument = {
+  description: string;
+  "dist-tags": Record<string, string>;
+  error?: unknown;
+  keywords: string[];
+  name: string;
+  versions: Record<string, Packument>;
+};
+
+export const load = (async ({ url, fetch }) => {
+  const paramPackages = await Promise.all(
+    url.searchParams.getAll("p").map(async (p) => {
+      const response = await fetch(`https://registry.npmjs.org/${p}`);
+      const paramPkg = (await response.json()) as Packument;
+
+      if (paramPkg.error) {
+        error(404, `${p} was not returned from npm registry api`);
+      }
+
+      const { keywords, name, description } =
+        paramPkg.versions[paramPkg["dist-tags"]["latest"]];
+
+      // const {keywords} = m
+
+      return {
+        name,
+        description,
+        keywords,
+      };
+    }),
+  );
+  return {
+    paramPackages,
+  };
+}) satisfies PageLoad;
