b78c0cd5691d2dab4b95a8743b055acf2777189c
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-07-26 16:00:47 -0500

    Add corner locations to the Porter embed page load

--- src/routes/(embeds)/porter/+page.ts
+++ src/routes/(embeds)/porter/+page.ts
@@ -1,5 +1,13 @@
-import type { PageLoad } from './$types';
+import type { PageLoad } from "./$types";
+import { Vector } from "$lib/shared/vector";
 
 export const load = (async () => {
-    return {};
+  const corners = [
+    { label: "N", loc: Vector.create(300, 120) },
+    { label: "E", loc: Vector.create(480, 300) },
+    { label: "S", loc: Vector.create(300, 480) },
+    { label: "W", loc: Vector.create(120, 300) },
+  ];
+
+  return { corners };
 }) satisfies PageLoad;
