bb32e61bbc6636c2dedc36ffe68cdf0c08575878
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-08-24 13:49:13 -0500

    Project cards (#70)
    * Refactor sorter layout and button grouping for improved UI
    
    * Remove unnecessary blank line in sorter component styles
    
    * Adjust textarea width for better responsiveness in sorter component
    
    * Fix favicon link to use absolute path for consistency

--- src/routes/(app)/utilities/+page.svelte
+++ src/routes/(app)/utilities/+page.svelte
@@ -15,22 +15,24 @@
     bind:value={sortable}
     placeholder="enter or paste lines to sort"
   ></textarea>
-  <button
-    class="button-a"
-    onclick={() => {
-      const lines = sortable.split("\n");
-      sortable = (dedup ? [...new Set(lines)] : lines).sort().join("\n");
-    }}>Sort</button
-  >
-  <div class="dedup">
-    <span>Deduplicate </span>
-    <input type="checkbox" bind:checked={dedup} />
+  <div class="bot">
+    <button
+      class="button-a"
+      onclick={() => {
+        const lines = sortable.split("\n");
+        sortable = (dedup ? [...new Set(lines)] : lines).sort().join("\n");
+      }}>Sort</button
+    >
+    <div>
+      <span>Deduplicate </span>
+      <input type="checkbox" bind:checked={dedup} />
+    </div>
+    <ClipboardCopy
+      width="5em"
+      textToCopy={sortable}
+      oncopied={(text) => console.log(text)}
+    />
   </div>
-  <ClipboardCopy
-    width="5em"
-    textToCopy={sortable}
-    oncopied={(text) => console.log(text)}
-  />
 </section>
 
 <style>
@@ -38,11 +40,22 @@
   input[type="checkbox"] {
     all: revert;
   }
+
+  textarea {
+    max-width: 40ch;
+  }
   .sorter {
     display: flex;
-    align-items: center;
+    margin: 2rem;
     flex-direction: column;
-    gap: 1em;
+    align-items: center;
+  }
+
+  .bot {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin: 1rem;
   }
 
   @media screen and (min-width: 576px) {
@@ -50,9 +63,6 @@
   }
   @media screen and (min-width: 768px) {
     /* tablets */
-    .sorter {
-      flex-direction: row;
-    }
   }
   @media screen and (min-width: 992px) {
     /* desktops */
